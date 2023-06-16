df31d2afd1dc857655655cebe8f46b97d404222b
charles-mrt <charlesmartins.ads@gmail.com>
2023-06-16 11:05:23 -0300

    added stars as background image

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+      <div className="bg-[url(../src/assets/bg-stars.svg)] flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
         
         {/* blur */}
         <div className= "absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full"/>
@@ -14,7 +14,7 @@ export default function Home() {
 
 
       {/* right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../src/assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
             Você não registrou nenhum momento, comece a {''}
