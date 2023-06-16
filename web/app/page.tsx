5dcd1423e611c0f875473c4a73ac405bd2522a96
charles-mrt <charlesmartins.ads@gmail.com>
2023-06-16 11:31:24 -0300

    created sign in field

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,16 +1,31 @@
+import { User } from 'lucide-react'
 
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
       <div className="bg-[url(../src/assets/bg-stars.svg)] flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
-        
+
         {/* blur */}
-        <div className= "absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full"/>
-        
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+
         {/* stripes */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"/>
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* sign in */}
+        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta </span>
+            e slave suas memórias!
+          </p>
+        </a>
+
       </div>
+
 
 
       {/* right */}
