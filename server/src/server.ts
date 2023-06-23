c211125d3e0710d33940b39c1aa513c67e1e0624
charles-mrt <charlesmartins.ads@gmail.com>
2023-06-23 11:47:45 -0300

    installed fastify cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import {memoriesRoutes} from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true,
+})
 
 /**
  * Registers the memories routes.
